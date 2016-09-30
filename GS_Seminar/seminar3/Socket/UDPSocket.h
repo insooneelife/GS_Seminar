370e2686b68543a50830f7225a1c397f60887067
insooneelife <insooneelife@naver.com>
2016-09-30 17:32:28 +0900

    add changes

--- GS_Seminar/seminar3/Socket/UDPSocket.h
+++ GS_Seminar/seminar3/Socket/UDPSocket.h
@@ -6,7 +6,7 @@
 #include "SocketAddress.h"
 #include "SocketUtil.h"
 
-// UDP 통신은 편지에 비유할 수 있다.
+// UDP 통신은 편지에 비유할 수 있다. 
 // packet -> 편지
 // socket -> 우체통
 // ip address -> 주소
@@ -18,8 +18,10 @@
 //	1. create socket(우체통)					1. create socket(우체통)
 //	2. bind(주소 등록)				 
 //	4. recvfrom(편지를 기다림)			<-		3. sendto(주소가 적힌 편지를 보냄)
-//	4. sendto(주소가 적힌 편지를 보냄)	->		5. recvfrom(편지를 기다림)
-//	5. close								6. close
+//	5. sendto(주소가 적힌 편지를 보냄)	->		6. recvfrom(편지를 기다림)
+//	7. close								7. close
+
+// UDP 통신을 편지에 비유하긴 했지만, 통신속도는 UDP가 더 빠르다.
 
 class UDPSocket
 {
